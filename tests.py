e0ded610dc00efc3758b67bdb90454df23bd3f87
ducdetronquito <guillaume.paulet@etu.univ-nantes.fr>
2016-12-13 20:09:02 +0100

    [Tests] Tested Clause class.

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from tests.database import *
 from tests.default_model import *
 from tests.manager import *
 from tests.queryset import *
+from tests.clause import *
 
 if __name__ == '__main__':
     unittest.main()
